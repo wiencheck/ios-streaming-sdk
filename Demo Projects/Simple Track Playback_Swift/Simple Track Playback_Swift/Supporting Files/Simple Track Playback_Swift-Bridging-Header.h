7612cf742f930eeb83eb8ea38f95014501b09f00
adam.wienconek <adamwienconek@gmail.com>
2018-10-24 15:14:45 +0200

    * Create project * Import essential frameworks

---
+++ Demo Projects/Simple Track Playback_Swift/Simple Track Playback_Swift/Supporting Files/Simple Track Playback_Swift-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyMetadata/SpotifyMetadata.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
---
+++ Demo Projects/Simple Track Playback_Swift/Simple Track Playback_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Track Playback_Swift
+//
+//  Created by adam.wienconek on 24.10.2018.
+//  Copyright © 2018 adam.wienconek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
